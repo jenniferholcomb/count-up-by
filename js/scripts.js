2f5098283e655edabd87452283222d83042c5c69
VW <34698193+QuietEvolver@users.noreply.github.com>
2022-10-26 11:21:33 -0700

    It will add a multiply by 5 fxn taking in only two vals: One to count[i] and the other to set the limit of the multiplier by.
    Co-authored-by: Jennifer Holcomb <jenniferlholcomb@gmail.com>.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+/*
+Create a program that takes two numbers — one to count to and another to determine what multiple to use to get there.
+
+Here is some sample input:
+Count to: 30
+Count by: 5
+Output: 5, 10, 15, 20, 25, 30
+
+Count to: 50
+Count by: 7
+Output: 7, 14, 21, 28, 35, 42, 49
+
+*/
+
+// one to count/another to determine multiple
+// Count to: 30
+// Count by: 5
+
+  let input = [30, 5];
+  let multOfFive = [];
+  for (let index = 0; index <= input[0]; index += 5) {
+    console.log(index);
+    multOfFive.push(index);
+  }
